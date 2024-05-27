420e64f5b7254ca9d0bc1211eb1cd54a193911ef
정기빈 <kibeen.jung@samsung.com>
2024-05-27 16:53:41 +0900

    jenkins

--- sort.py
+++ sort.py
@@ -1,3 +1,17 @@
-print("sort test")
+import unittest
+import xmlrunner
 
-print("sort test")
+class MyTest(unittest.TestCase):
+    def test_example1(self):
+        self.assertEqual(1, 1)
+        
+    def test_example2(self):
+        self.assertEqual(2, 1)
+        
+    def test_example3(self):
+        self.assertEqual(6, 6)
+
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
